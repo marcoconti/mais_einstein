18f776e5cc0d87a9d27ff6a4964b1447c9a0f274
Marco Aurelio Conti <marco.conti@einstein.br>
2026-07-27 08:35:10 -0300

    Atualização repositório

---
+++ spark_utils.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from pyspark.sql import SparkSession
+
+def get_spark_session(app_name="NotebookSession", driver_mem="8g", exec_mem="8g") -> SparkSession:
+    """
+    Cria e retorna uma SparkSession configurada para ambiente local.
+    """
+    # 1. Remove qualquer barreira de proxy local
+    for env_var in ['HTTP_PROXY', 'HTTPS_PROXY', 'http_proxy', 'https_proxy']:
+        os.environ.pop(env_var, None)
+
+    # 2. Garante que o Spark use o Python do venv ativo
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+    # 3. Força o IP local
+    os.environ['SPARK_LOCAL_IP'] = '127.0.0.1'
+
+    # 4. Inicializa o Spark
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .master("local[*]") \
+        .config("spark.driver.host", "127.0.0.1") \
+        .config("spark.driver.bindAddress", "127.0.0.1") \
+        .config("spark.network.auth.enabled", "false") \
+        .config("spark.driver.memory", driver_mem) \
+        .config("spark.executor.memory", exec_mem) \
+        .getOrCreate()
+
+    return spark
