cd78d0bc9cc03f590341c4321f22d62c21f36203
Marco Conti <marcoaurelioconti@yahoo.com.br>
2026-08-10 13:23:03 -0300

    Atualização 2026-08-10 13:00

--- spark_utils.py
+++ spark_utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import xarray as xr
 from pyspark.sql import SparkSession
 
 def get_spark_session(app_name="NotebookSession", driver_mem="8g", exec_mem="8g") -> SparkSession:
@@ -30,3 +31,23 @@ def get_spark_session(app_name="NotebookSession", driver_mem="8g", exec_mem="8g"
    
 
     return spark
+
+def write_data_csv(df_write, write_path, file_name):
+    
+    df_write.toPandas().to_csv(f"{write_path}\{file_name}", index=False)
+
+
+def convert_nc_to_spark_dataframe(spark, path, file_name):
+    with xr.open_dataset(f"{path}\{file_name}"
+                        ,engine="netcdf4"
+                        ,chunks={"time": 365
+                                ,"latitude": 100
+                                ,"longitude": 100 }
+                        ) as ds:
+        
+        # Transforma o Dataset em um Spark Dataframe
+        df_dask     = ds.to_dask_dataframe()
+        df_dask_c   = df_dask.compute()
+        df_spark    = spark.createDataFrame(df_dask_c)
+
+    return df_spark
